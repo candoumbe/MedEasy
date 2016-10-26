237d478092c9d236b2b5177e09f7de13e85af4bf
Cyrille NDOUMBE <ndoumbecyrille@hotmail.com>
2016-10-27 01:16:28 +0200

    Added unit tests for ValidateModelAttribute
    Éléments de travail associés : #1

---
+++ test/MedEasy.API.Tests/Filters/ValidateModelAttributeTests.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using MedEasy.API.Filters;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Routing;
+using Moq;
+using System.Collections.Generic;
+using Xunit;
+using static Moq.MockBehavior;
+
+namespace MedEasy.API.Tests.Filters
+{
+    public class ValidateModelAttributeTests
+    {
+        [Fact]
+        public void ShouldReturnBadRequesWhenModelStateIsNotValid()
+        {
+            // Arrange
+            ModelStateDictionary modelState = new ModelStateDictionary();
+            modelState.AddModelError("name", "invalid");
+
+            ActionContext actionContext = new ActionContext(
+               new Mock<HttpContext>().Object,
+               new Mock<RouteData>().Object,
+               new Mock<ActionDescriptor>().Object,
+               modelState);
+
+            var actionExecutingContext = new ActionExecutingContext(
+                actionContext,
+                new List<IFilterMetadata>(),
+                new Dictionary<string, object>(),
+                new Mock<Controller>());
+
+
+            
+            // Act
+            ValidateModelAttribute attribute = new ValidateModelAttribute();
+            attribute.OnActionExecuting(actionExecutingContext);
+
+            // Assert
+            actionExecutingContext.Result.Should()
+                .NotBeNull().And
+                .BeAssignableTo<BadRequestObjectResult>();
+        }
+    }
+}
