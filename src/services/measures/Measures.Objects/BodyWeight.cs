f623b264ee84467017080eee240a57ed8bddcb4a
Cyrille-Alexandre NDOUMBE <ndoumbecyrille@hotmail.com>
2020-05-15 22:40:51 +0200

    Adds unit tests for Temperature / BodyWeight / BloodPressure

--- src/services/measures/Measures.Objects/BodyWeight.cs
+++ src/services/measures/Measures.Objects/BodyWeight.cs
@@ -3,19 +3,54 @@
 namespace Measures.Objects
 {
     /// <summary>
-    /// An instance of this class represents a body weight measurement
+    /// An instance of this class represents a body weight measurement.
+    /// 
+    /// <para>
+    /// <see cref="Value"/> is expressed in the international unit system : the Kilogram (see https://en.wikipedia.org/wiki/Kilogram)
+    /// </para>
     /// </summary>
     public class BodyWeight : PhysiologicalMeasurement, IEquatable<BodyWeight>
     {
         /// <summary>
-        /// Gets / sets the temperature
+        /// Gets the current value of the messure (expressed in Kilogram)
         /// </summary>
-        public decimal Value { get; set; }
+        public float Value { get; private set; }
 
-        public BodyWeight(Guid id, Guid patientId, DateTime dateOfMeasure, decimal value)
+        /// <summary>
+        /// Builds a new <see cref="BodyWeight"/> instance.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="patientId"></param>
+        /// <param name="dateOfMeasure"></param>
+        /// <param name="value"></param>
+        public BodyWeight(Guid id, Guid patientId, DateTime dateOfMeasure, float value)
            : base(id, patientId, dateOfMeasure)
         {
+            if (float.IsNaN(value) || float.IsPositiveInfinity(value) || value < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(value), value, ValueIsOutsideRangeOfValidValuesMessage);
+            }
+
             Value = value;
+        }
+
+        /// <summary>
+        /// Updates <see cref="Value"/>.
+        /// </summary>
+        /// <param name="newValue"></param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        ///     if either :
+        /// <list type="bullet">
+        ///         
+        /// </list>
+        /// </exception>
+        public void ChangeValueTo(float newValue)
+        {
+            if (float.IsNaN(newValue) || float.IsPositiveInfinity(newValue) || newValue < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(newValue), newValue, ValueIsOutsideRangeOfValidValuesMessage);
+            }
+            Value = newValue;
         }
 
         public bool Equals(BodyWeight other) => ReferenceEquals(this, other) || (other != null && Value.Equals(other.Value));
---
+++ src/services/measures/tests/Measures.Objects.Tests/TemperatureTests.cs
@@ -0,0 +1,136 @@
+﻿using Bogus;
+
+using FluentAssertions;
+using FluentAssertions.Extensions;
+
+using System;
+using System.Collections.Generic;
+
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Categories;
+
+namespace Measures.Objects.UnitTests
+{
+    [UnitTest]
+    [Feature(nameof(Measures))]
+    [Feature(nameof(Temperature))]
+    public class TemperatureTests
+    {
+        private readonly ITestOutputHelper _outputHelper;
+        private readonly static Faker _faker;
+
+        static TemperatureTests()
+        {
+            _faker = new Faker();
+        }
+
+        public TemperatureTests(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+
+        public static IEnumerable<object[]> CtorArgumentOutOfRangeExceptionCases
+        {
+            get
+            {
+                yield return new object[] { Guid.Empty, Guid.NewGuid(), 12.December(2013), 8, "Measure is empty" };
+                yield return new object[] { Guid.NewGuid(), Guid.Empty, 12.December(2013), 8, "Patient id is empty" };
+                yield return new object[] { Guid.NewGuid(), Guid.NewGuid(), DateTime.MinValue, 8, "Date of measure is DateTime.MinValue" };
+                yield return new object[] { Guid.NewGuid(), Guid.NewGuid(), 14.January(1985), float.PositiveInfinity, "value is PositiveInfinity." };
+                yield return new object[] { Guid.NewGuid(), Guid.NewGuid(), 14.January(1985), float.NaN, "value is NaN." };
+                yield return new object[] { Guid.NewGuid(), Guid.NewGuid(), 14.January(1985), _faker.Random.Float(min: float.MinValue, max:Temperature.LowestKnownTemperature), "value is lower that lowest measurable temperature." };
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(CtorArgumentOutOfRangeExceptionCases))]
+        public void Ctor_throws_ArgumentOutOfRangeException_when_argument_is_out_of_expected_range(Guid id, Guid patientId, DateTime dateOfMeasure, float value, string reason)
+        {
+            // Arrange
+            _outputHelper.WriteLine($"{nameof(patientId)} : '{patientId}'");
+            _outputHelper.WriteLine($"{nameof(id)} : '{id}'");
+            _outputHelper.WriteLine($"{nameof(dateOfMeasure)} : '{dateOfMeasure}'");
+            _outputHelper.WriteLine($"{nameof(value)} : '{value}'");
+
+            // Act
+            Action ctorWithInvalidArgument = () => new Temperature(id, patientId, dateOfMeasure, value);
+
+            // Assert
+            ctorWithInvalidArgument.Should()
+                                   .ThrowExactly<ArgumentOutOfRangeException>(reason);
+        }
+
+        [Fact]
+        public void Ctor_can_builds_measure_with_zero()
+        {
+            // Act
+            Temperature measure = new Temperature(Guid.NewGuid(), Guid.NewGuid(), 13.January(2009), 0);
+
+            // Assert
+            measure.Value.Should()
+                         .Be(0);
+        }
+
+        public static IEnumerable<object[]> ChangeValueInvalidCases
+        {
+            get
+            {
+                Temperature measure = new Temperature(Guid.NewGuid(), Guid.NewGuid(), 13.January(2009), 8);
+
+                yield return new object[]
+                {
+                    measure,
+                    float.NegativeInfinity,
+                    "new value is negative infinity"
+                };
+
+                yield return new object[]
+                {
+                    measure,
+                    float.NaN,
+                    $"new value is {float.NaN}"
+                };
+
+                yield return new object[]
+                {
+                    measure,
+                    float.PositiveInfinity,
+                    $"new value is {float.PositiveInfinity}"
+                };
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(ChangeValueInvalidCases))]
+        public void ChangeValue_with_incorrect_value_throws_ArgumentOutOfRangeException(Temperature measure, float newValue, string reason)
+        {
+            // Act
+            Action changeValue = () => measure.ChangeValueTo(newValue);
+
+            // Assert
+            changeValue.Should()
+                       .ThrowExactly<ArgumentOutOfRangeException>(reason)
+                       .Where(ex => ex.ParamName != null)
+                       .Where(ex => ex.ActualValue != null, "the value that causes the exception to be thrown should be advertised ")
+                       .Where(ex => !string.IsNullOrWhiteSpace(ex.Message));
+        }
+
+        [Fact]
+        public void ChangeValue_sets_Value()
+        {
+            // Arrange
+            Temperature measure = new Temperature(Guid.NewGuid(), Guid.NewGuid(), 13.January(2009), 0);
+            float newValue = _faker.Random.Float(min: Temperature.LowestKnownTemperature, max: float.MaxValue);
+
+            _outputHelper.WriteLine($"Value to set : {newValue}");
+
+            // Act
+            measure.ChangeValueTo(newValue);
+
+            // Assert
+            measure.Value.Should()
+                         .Be(newValue);
+        }
+    }
+}
