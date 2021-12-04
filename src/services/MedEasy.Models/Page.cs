6dfc52b542204a587ec642d0d78ffb3d741f0c97
Cyrille-Alexandre NDOUMBE <ndoumbecyrille@hotmail.com>
2021-12-04 12:48:17 +0100

    Move to `net6.0`

---
+++ src/services/MedEasy.Models/Page.cs
@@ -0,0 +1,15 @@
+﻿namespace MedEasy.Models;
+using System.Collections.Generic;
+
+public class PageModel<T> where T : notnull
+{
+    public int Page { get; set; }
+
+    public int Total { get; set; }
+
+    public int Count { get; set; }
+
+    public int PageSize { get; set; }
+
+    public IEnumerable<T> Items { get; set; }
+}
---
+++ src/services/identity/refit/Identity.Api.Refit/v1/IAuthApi.cs
@@ -0,0 +1,47 @@
+﻿using Identity.DTO;
+using Identity.DTO.Auth;
+using Identity.DTO.v1;
+using Identity.Ids;
+
+using MedEasy.RestObjects;
+
+using Refit;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Identity.Api.Refit.v1;
+
+/// <summary>
+/// Authentication API v1
+/// </summary>
+public interface IAuthApi
+{
+    /// <summary>
+    /// Connects the user with the specified <paramref name="login"/>
+    /// </summary>
+    /// <param name="login">Contains username and password of the user to authenticate</param>
+    /// <param name="ct"></param>
+    /// <returns></returns>
+    [Post("/v1/auth/token")]
+    Task<ApiResponse<BearerTokenInfo>> Login([Body]LoginInfo login, CancellationToken ct);
+
+    /// <summary>
+    /// Disconnects the user with the specified <see cref="username"/>
+    /// </summary>
+    /// <param name="login">Contains username and password of the user to authenticate</param>
+    /// <param name="ct"></param>
+    /// <returns></returns>
+    [Delete("/v1/auth/token/{username}")]
+    Task Disconnect(string username, CancellationToken ct);
+
+    /// <summary>
+    /// Ask for a new refresh token for the user with the specified name.
+    /// </summary>
+    /// <param name="username"></param>
+    /// <param name="refresh"></param>
+    /// <param name="ct"></param>
+    /// <returns></returns>
+    [Put("/v1/auth/token/{username}")]
+    Task<ApiResponse<BearerTokenInfo>> Refresh(string username, [Body] RefreshAccessTokenInfo refresh, CancellationToken ct);
+}
